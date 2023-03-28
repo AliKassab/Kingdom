43eec2e29917428af57bb2466573f2ba937ee813
Ali Kassab <alilool99335@gmail.com>
2023-03-28 14:14:07 +0200

    Ram Movement

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] float movementDelay = 1f;
+    [SerializeField] [Range(0,5)] float speed = 1f;
+
+    Vector3 startPosition;
+    Vector3 endPosition;
+    float travelPercent = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,9 +21,19 @@ public class EnemyMover : MonoBehaviour
     {
         foreach (Waypoint waypoint in path)
         {
-            transform.position = waypoint.transform.position;
+            startPosition = transform.position;
+            endPosition = waypoint.transform.position;
+            travelPercent = 0f;
 
-            yield return new WaitForSeconds(movementDelay);
+            transform.LookAt(endPosition);
+
+            while (travelPercent < 1f)
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 }
